e3f58c68ed63b8d9ebd3f4592b3dc199f97c71f0
Dorian Hidalgo <dorianhl5229@gmail.com>
2024-10-27 22:25:45 -0400

    host rename

--- app.py
+++ app.py
@@ -39,4 +39,4 @@ async def url():
 # Documentation with Swagger: http://127.0.0.1:8000/docs
 # Documentation with Redocly: http://127.0.0.1:8000/redoc
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0")
